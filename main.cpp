7b0f303c4de1be2ee939bfec2e72abc76e5e3923
Kiarash Jalali <73154381+kiajll@users.noreply.github.com>
2025-01-17 17:54:29 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <cstring>
 #include <complex>
 #include <fftw3.h>
 #include <fstream>
@@ -9,24 +10,24 @@
 using namespace std;
 using namespace chrono;
 
-////// Function prototypes
+//################### 00. Function Prototype Declarations ####################
 void compute_rhs(double a, double nu, double Lx, const vector <double> &kx, const fftw_complex *u_hat, vector<complex<double>> &du_hat_dt);
 
 int main(){
 
-// 1.Parameters
+// 0.Parameters
     const double nu{0.001};                   // Kinematic viscosity
-    const int N = pow(2,9);                   // Number of spatial grid points, degree of freedom
+    const int N = pow(2,8);            // Number of spatial grid points, degree of freedom
     const double U0{1.0};                     // Amplitude of the initial velocity field
     const double a{1.0};                      // Convective velocity
     const int n{1};                           // Mode of initial wavenumber
     const double Lx{2.0*M_PI};                // Length of periodic domain
     const double k = (n* 2 * M_PI / Lx);      // Wave number of initial velocity field
     const double tf = 1;                      // Final time
-    const double dt = 1e-5;                   // Time step
-    const int Nt = static_cast<int>(tf / dt); // Number of time steps
+    const double dt = 1e-4;                   // Time step
+    const size_t Nt = static_cast<size_t>(round(tf / dt)); // Number of time steps
 
-//// 0. CFL evaluation    
+// 1.CFL evaluation    
 double dx = Lx / N;
 double CFL = a * dt / dx;
 cout << "CFL number equals to: " << CFL <<endl;
@@ -62,69 +63,80 @@ if (CFL > 1.0) {
         outFile1 << x[i] << "," << u[i] << "\n";
     }
     outFile1.close();
-        
-    // 3.3 Fourier transform of Initial data
-    fftw_complex *u_hat;
-    fftw_complex *du_hat_dt;
 
-// 4. Allocate memory for Fourier coefficients
-    size_t fft_size = sizeof(fftw_complex) * N;
-    u_hat = (fftw_complex*) fftw_malloc(fft_size);    // u_hat is a pointer of the type fftw_complex
-    du_hat_dt = (fftw_complex*) fftw_malloc(fft_size);
-
-// 5. Create FFTW plans
-    fftw_plan forward = fftw_plan_dft_r2c_1d(N, u.data(), u_hat, FFTW_ESTIMATE);
-    fftw_plan backward = fftw_plan_dft_c2r_1d(N, u_hat, u.data(), FFTW_ESTIMATE);
     
-// 6. Perform forward FFT
-    fftw_execute(forward);
-    
-// 7. Define wave numbers (kx)
+//################### 02. Initialize FFTW PLAN ####################
+// 02.01. Define wave numbers (kx)
     vector <double> kx (N);
     for (int i = 0; i < N; ++i) {
         kx[i] = (i < N / 2) ? i * 2.0 * M_PI / Lx : (i - N) * 2.0 * M_PI / Lx;
     }    
-    
-// 8. Allocate memory for intermediate arrays
-    fftw_complex *u_hat1 = (fftw_complex*) fftw_malloc(fft_size);
-    fftw_complex *u_hat2 = (fftw_complex*) fftw_malloc(fft_size);
-    fftw_complex *u_hat3 = (fftw_complex*) fftw_malloc(fft_size);
+ 
+// 02.02 Fourier transform of Initial data
+    fftw_complex *u_hat;
+    fftw_complex *du_hat_dt;
 
-// 8.1. Check memory allocation success for intermediate arrays
+// 02.03. Allocate memory for Fourier coefficients
+    size_t fft_size = sizeof(fftw_complex) * N;
+    
+    u_hat = (fftw_complex*) fftw_malloc(fft_size);    // u_hat is a pointer of the type fftw_complex
+    memset(u_hat, 0, fft_size);  //  Zero Initialize
+
+    du_hat_dt = (fftw_complex*) fftw_malloc(fft_size);
+    memset(du_hat_dt, 0, fft_size);  //  Zero Initialize
+
+// 02.04. Create FFTW plans
+    fftw_plan forward = fftw_plan_dft_r2c_1d(N, u.data(), u_hat, FFTW_ESTIMATE);
+    fftw_plan backward = fftw_plan_dft_c2r_1d(N, u_hat, u.data(), FFTW_ESTIMATE);
+    
+// 02.05. Perform forward FFT
+    fftw_execute(forward);
+   
+// 02.06. Allocate memory for intermediate arrays
+    fftw_complex *u_hat1 = (fftw_complex*) fftw_malloc(fft_size);
+    memset(u_hat1, 0, fft_size);  //  Zero Initialize
+    
+    fftw_complex *u_hat2 = (fftw_complex*) fftw_malloc(fft_size);
+    memset(u_hat2, 0, fft_size);
+
+    fftw_complex *u_hat3 = (fftw_complex*) fftw_malloc(fft_size);
+    memset(u_hat3, 0, fft_size);
+// 02.06.01. Check memory allocation success for intermediate arrays
     if (!u_hat1 || !u_hat2 || !u_hat3) {
         cerr << "Error: Memory allocation failed for intermediate arrays (u_hat1 or u_hat2). Exiting program." << endl;
         return -1;
     }
-//  8. Allocate intermediate arrays to improve performance 
+    
+//  02.07. Allocate intermediate arrays to improve performance 
     vector<complex<double>> R1(kx.size());
     vector<complex<double>> R2(kx.size());
     vector<complex<double>> R3(kx.size());
-//########################################################################################
-// Start time      
+
+//####################### 03. Processing Stage ##########################    
+// 03.01. Main time-stepping loop   
 auto start = high_resolution_clock::now();
     
-// 9. Time-stepping loop
-for (int t_cuntr = 0; t_cuntr < Nt; ++t_cuntr){
-
-// 9.1.1 RK3 stage 1
+// 03.02. Time-stepping loop
+for (size_t t_cuntr{0}; t_cuntr < Nt+1; ++t_cuntr){
+// 03.02.01. RK3 stage 1
     memcpy(u_hat1, u_hat, fft_size);
     compute_rhs(a, nu, Lx, kx, u_hat1, R1);
     
-// 9.1.2 RK3 stage 2
+// 03.02.02. RK3 stage 2
     for (int i = 0; i < N; ++i) {
             u_hat2[i][0] = u_hat[i][0] + 0.5 * dt * R1[i].real();
             u_hat2[i][1] = u_hat[i][1] + 0.5 * dt * R1[i].imag();
     }
     compute_rhs(a, nu, Lx, kx, u_hat2, R2);
     
-// 9.1.3 RK3 stage 3
+// 03.02.03. RK3 stage 3
     for (int i = 0; i < N; ++i) {
             u_hat3[i][0] = u_hat[i][0] + 0.75 * dt * R2[i].real();
             u_hat3[i][1] = u_hat[i][1] + 0.75 * dt * R2[i].imag();
     }
     compute_rhs(a, nu, Lx, kx, u_hat3, R3);
     
-// 9.2. Final stage
+// 03.02.04. Final stage
     for(int i = 0; i < N; ++i){
         u_hat[i][0] += (dt / 9.0) * (2.0 * R1[i].real() + 3.0 * R2[i].real() + 4.0 * R3[i].real() );
         u_hat[i][1] += (dt / 9.0) * (2.0 * R1[i].imag() + 3.0 * R2[i].imag() + 4.0 * R3[i].imag());
@@ -133,29 +145,29 @@ for (int t_cuntr = 0; t_cuntr < Nt; ++t_cuntr){
 }
 //##################################################################################
 
-// 9.3. Transform back to Physical space
+// 03.03. Transform back to Physical space
     fftw_execute(backward);
     
-// 9.4. Enforcing final step periodic condition
+// 03.04. Enforcing final step periodic condition
     u[N-1] = u[0];
   
-// 9.5. Normalize Final result
+// 03.05. Normalize Final result
     for (int i=0; i< N; ++i){
         u[i] /= N;
     }
-// 10. Clean up
+// 03.06. Clean up
     fftw_destroy_plan(forward);
     fftw_destroy_plan(backward);
     fftw_free(u_hat);
     fftw_free(du_hat_dt);
     
-// 11. Output results
+// 03.07.01. Output results
 cout << "\n ## Final results ## " << endl;
 for (int i = 0; i < N; ++i) {
     cout << "x = " << x[i] << ", u = " << u[i] << endl;
 }
     
-// 12. Write data to a CSV file
+// 03.07.02. Write data to a CSV file
    ofstream outFile2("velocity_field_final.csv");  //ofstream is a class from <fstream> library
     if (!outFile2) {
         cerr << "Error: Could not open file for writing!" << endl;
@@ -173,15 +185,17 @@ for (int i = 0; i < N; ++i) {
     outFile2.close();
     cout << "\nFile 'velocity_field.csv' written successfully!" << endl;
 
-// End measuring time
+// 03.08. End measuring time
 auto end = chrono::high_resolution_clock::now();
 auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
 cout << "Elapsed time for the time-stepping loop: " << duration.count() << " ms" << endl;    
 
 return 0;
 }
-//#####################################################################################
-// 15. Function prototype definition, rhs function: du_hat_dt = compute_rhs(nu,Lx,k,u_hat)
+
+
+//################### 04. Function Definitions #################### 
+// 04.01 rhs function: du_hat_dt = compute_rhs(nu,Lx,k,u_hat)
 void compute_rhs(double a, double nu, double Lx, const vector <double> &kx, const fftw_complex *u_hat, vector<complex<double>> &du_hat_dt){
     int N = kx.size();
 
